ee68c76e70e1366da2f4207158c439ab574c31b4
spartrekus <spartrekus@gmail.com>
2019-02-22 20:45:57 +0100

    Add files via upload

--- lkm.c
+++ lkm.c
@@ -49,6 +49,7 @@
 
 int rows, cols ; 
 int pansel = 1;
+char userstr[PATH_MAX];
 
 #define KNRM  "\x1B[0m"
 #define KRED  "\x1B[31m"
@@ -138,30 +139,6 @@ return fileordir;
 }
 
 
-
-int  fiche = 0 ; 
-
-///////////////
-char* scan_line( char* buffer, int buffer_size) 
-{
-   char* p = buffer;
-   int count = 0;
-   do {
-       char c;
-       scanf("%c", &c); // scan a single character
-       // break on end of line, string terminating NUL, or end of file
-       if (c == '\r' || c == '\n' || c == 0 || c == EOF) 
-       {
-           *p = 0;
-           break;
-       }
-
-       *p++ = c; // add the valid character into the buffer
-   } while (count < buffer_size - 1);  // don't overrun the buffer
-   // ensure the string is null terminated
-   buffer[buffer_size - 1] = 0;
-   return buffer;
-}
 
 
 ///////////////////////////////////////////
@@ -301,10 +278,109 @@ void printdir( int pyy, int fopxx, char *mydir , int panviewpr )
    //mvprintw( rows-1, cols/2, "[FILE: %s]", nexp_user_fileselection );
 }
 
+////////////////////////////////////////////////////////////////////
+char *strcut( char *str , int myposstart, int myposend )
+{     // copyleft, C function made by Spartrekus 
+      char ptr[strlen(str)+1];
+      int i,j=0;
+      for(i=0; str[i]!='\0'; i++)
+      {
+        if ( ( str[i] != '\0' ) && ( str[i] != '\0') )
+         if ( ( i >=  myposstart-1 ) && (  i <= myposend-1 ) )
+           ptr[j++]=str[i];
+      } 
+      ptr[j]='\0';
+      size_t siz = sizeof ptr ; 
+      char *r = malloc( sizeof ptr );
+      return r ? memcpy(r, ptr, siz ) : NULL;
+}
 
 
 
 
+
+
+/////////////////////////////////////
+void strninput( char *mytitle, char *foostr )
+{
+      strncpy( userstr , "" , PATH_MAX );
+      disable_waiting_for_enter();
+      char strmsg[PATH_MAX];
+      char charo[PATH_MAX];
+      strncpy( strmsg, foostr , PATH_MAX );
+      int foousergam = 0; int ch ;  int chr;
+      while( foousergam == 0 ) 
+      {
+
+         home();
+         for ( chr = 0 ;  chr <= cols-1 ; chr++) printf( " ");
+
+         home();
+         printf( "|%s|", mytitle );
+
+         ansigotoyx( rows, 0 );
+         for ( chr = 0 ;  chr <= cols-1 ; chr++) printf( " ");
+         ansigotoyx( rows, 0 );
+         printf( ": %s", strmsg );
+
+         ch = getchar();
+         if ( ch == 10 )       foousergam = 1;
+
+	 else if ( ch == 27 ) 
+         {
+              foousergam = 1;
+	      strncpy( strmsg, ""  ,  PATH_MAX );
+         }
+
+         else if ( ch == 127 ) 
+            strncpy( strmsg, strcut( strmsg, 1 , strlen( strmsg ) -1 )  ,  PATH_MAX );
+
+	 else if (
+			(( ch >= 'a' ) && ( ch <= 'z' ) ) 
+		        || (( ch >= 'A' ) && ( ch <= 'Z' ) ) 
+		        || (( ch >= '1' ) && ( ch <= '9' ) ) 
+		        || (( ch == '0' ) ) 
+		        || (( ch == '~' ) ) 
+		        || (( ch == '!' ) ) 
+		        || (( ch == '&' ) ) 
+		        || (( ch == '=' ) ) 
+		        || (( ch == ':' ) ) 
+		        || (( ch == ';' ) ) 
+		        || (( ch == '<' ) ) 
+		        || (( ch == '>' ) ) 
+		        || (( ch == ' ' ) ) 
+		        || (( ch == '|' ) ) 
+		        || (( ch == '#' ) ) 
+		        || (( ch == '?' ) ) 
+		        || (( ch == '+' ) ) 
+		        || (( ch == '/' ) ) 
+		        || (( ch == '\\' ) ) 
+		        || (( ch == '.' ) ) 
+		        || (( ch == '$' ) ) 
+		        || (( ch == '%' ) ) 
+		        || (( ch == '-' ) ) 
+		        || (( ch == ',' ) ) 
+		        || (( ch == '{' ) ) 
+		        || (( ch == '}' ) ) 
+		        || (( ch == '(' ) ) 
+		        || (( ch == ')' ) ) 
+		        || (( ch == ']' ) ) 
+		        || (( ch == '[' ) ) 
+		        || (( ch == '*' ) ) 
+		        || (( ch == '"' ) ) 
+		        || (( ch == '@' ) ) 
+		        || (( ch == '-' ) ) 
+		        || (( ch == '_' ) ) 
+		        || (( ch == '^' ) ) 
+		        || (( ch == '\'' ) ) 
+	             ) 
+		  {
+                        snprintf( charo, PATH_MAX , "%s%c",  strmsg, ch );
+		        strncpy( strmsg,  charo ,  PATH_MAX );
+		  }
+     }
+     strncpy( userstr, strmsg , PATH_MAX );
+}
 
 
 
@@ -426,13 +502,15 @@ int main( int argc, char *argv[])
             chdir( pathpan[ pansel ] );
             strncpy( pathpile , getcwd( cwd, PATH_MAX ), PATH_MAX );
        }
-       else if ( ch == '\'')      
+
+       else if ( ( ch == '#')  || ( ch == '\'')  )
        {
             chdir( pathpan[ pansel ] );
             chdir( pathpile );
-            nexp_user_sel[pansel]=1; nexp_user_scrolly[pansel] = 0; 
             strncpy( pathpan[ pansel ] , getcwd( cwd, PATH_MAX ), PATH_MAX );
+            nexp_user_sel[pansel]=1; nexp_user_scrolly[pansel] = 0; 
        }
+
        else if ( ch == 'w')      
        {
             chdir( pathpan[ pansel ] );
@@ -492,7 +570,6 @@ int main( int argc, char *argv[])
        else if ( ch == '4' )   {  enable_waiting_for_enter();  nrunwith(  " vim  ",  nexp_user_fileselection    );   }
        else if ( ch == 'e' )   {  enable_waiting_for_enter();  nrunwith(  "  tless  ",  nexp_user_fileselection    );   }
        else if ( ch == 'z' )   {  enable_waiting_for_enter();  nrunwith(  " less  ",  nexp_user_fileselection    );   }
-       else if ( ch == '$' )     nsystem( " sh  ");
        else if ( ch == '0' )
        {  viewpan[ 1 ] = 0;  viewpan[ 2 ] = 0; }
        else if ( ch == '1' )
@@ -504,31 +581,91 @@ int main( int argc, char *argv[])
        else if ( ch == 'i' )
        {  if ( pansel == 1 )   pansel = 2 ; else pansel = 1; }
 
-        else if (ch == '7') 
-        {
+       else if ( ch == '?') 
+       {
             enable_waiting_for_enter();
             clear_screen_retro();
-            printf( "" );
-            printf( "" );
-            printf("Enter your dirname: ");
-            scanf("%s", string ); 
-            if ( strcmp( string , "" ) != 0 ) nrunwith( " mkdir " , string ) ; 
+            printf( "======= \n" );
+            printf( "= LFM = \n" );
+            printf( "======= \n" );
+            printf( " \n" );
+            printf( "LFM is a twin pan file manager, for FreeBSD, NetBSD, and Linux.\n" );
+            printf( " \n" );
+            printf( " Author: Spartrekus\n" );
+            printf( " Author OS: NetBSD\n" );
+            disable_waiting_for_enter();
+            getchar();
+        }
+
+
+
+
+       else if ( ch == 's') 
+       {
+            enable_waiting_for_enter();
+            clear_screen_retro();
+            printf( "======= \n" );
+            printf( "= LFM = \n" );
+            printf( "======= \n" );
+            printf( " \n" );
+            printf( " \n" );
+            printf("PATH:  %s \n", getcwd( cwd, PATH_MAX) );
+            printf( " \n" );
+            printf("PATH1: %s \n", pathpan[ 1 ] );
+            printf( " \n" );
+            printf("PATH2: %s \n", pathpan[ 2 ] );
+            printf( " \n" );
+            printf("PATH PILE: %s", pathpile );
+            printf( " \n" );
+            getchar();
             disable_waiting_for_enter();
         }
+
 
 
         else if (ch == ':') 
         {
-            clrscr();
-            home();
-            enable_waiting_for_enter();
-            strncpy( string, "" , PATH_MAX );
-            printf("Enter a string: ");
-            scan_line( string , PATH_MAX);
+            strninput( "CMD:", "" );
+            strncpy( string, userstr , PATH_MAX );
+            printf("\n" );
+            printf("\n" );
             printf("got: \"%s\"\n", string );
-            disable_waiting_for_enter();
+            getchar();
         }
 
+
+        else if (ch == '7') 
+        {
+            strninput( " Create Directory (MKDIR) ", "" );
+            strncpy( string, userstr , PATH_MAX );
+            printf("\n" );
+            printf("\n" );
+            printf("got: \"%s\"\n", string );
+            if ( strcmp( string , "" ) != 0 ) nrunwith( " mkdir " , string ) ; 
+        }
+
+
+        else if (ch == '!') 
+        {
+            strninput( " Run Cmd on File (!) ", "" );
+            strncpy( string, userstr , PATH_MAX );
+            printf("\n" );
+            printf("\n" );
+            printf("got: \"%s\"\n", string );
+            if ( strcmp( string , "" ) != 0 ) 
+               nrunwith( string , nexp_user_fileselection  ) ; 
+        }
+
+       else if ( ch == '$' ) 
+        {
+            strninput( " Run Bash Command ", "" );
+            strncpy( string, userstr , PATH_MAX );
+            printf("\n" );
+            printf("\n" );
+            printf("got: \"%s\"\n", string );
+            if ( strcmp( string , "" ) != 0 ) 
+               nsystem( string );
+        }
 
     }
 
